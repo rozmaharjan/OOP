d280d0ddfae6c812ec80b7a5942e32afa03a9692
rozmaharjan <rox.maharjan@gmail.com>
2022-04-19 21:40:50 +0545

    conditional statrmemt

---
+++ Conditionalstatement/src/Alphabet.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Alphabet {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a Character: ");
+        char alphabet = sc.next().charAt(0);
+
+        if((alphabet>='A' && alphabet<='z') || (alphabet>='a' && alphabet<='z'))
+        {
+            System.out.println(" It is an Alphabet.");
+        }
+        else
+        {
+            System.out.println(" It is not an Alphabet.");
+        }
+    }
+}
